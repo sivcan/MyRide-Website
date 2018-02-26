5dd1a86137ca898ac6b24c1e0adea7e95e6455c0
sivcan.singh <sivcan@getpostman.com>
2018-02-27 00:48:28 +0530

    Redirect route

--- flaskapp.py
+++ flaskapp.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
+
+@app.route("/jobs")
+def jobs():
+	return redirect("https://waechterw.wixsite.com/fahrerjob", code=301)
 
 @app.route("/")
 def index():
